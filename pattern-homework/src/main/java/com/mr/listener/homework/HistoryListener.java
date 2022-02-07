ff1ab287f05fd051c2bc925f337e987da920b57d
Rezchikov_Maksim <erkenbrand13@yandex.ru>
2022-02-07 14:06:39 +0300

    Pattern homework - init commit

---
+++ pattern-homework/src/main/java/com/mr/listener/homework/HistoryListener.java
@@ -0,0 +1,19 @@
+package com.mr.listener.homework;
+
+import com.mr.listener.Listener;
+import com.mr.model.Message;
+
+import java.util.Optional;
+
+public class HistoryListener implements Listener, HistoryReader {
+
+    @Override
+    public void onUpdated(Message msg) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Optional<Message> findMessageById(long id) {
+        throw new UnsupportedOperationException();
+    }
+}
